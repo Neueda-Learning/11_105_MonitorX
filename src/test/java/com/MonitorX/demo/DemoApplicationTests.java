9a300ce42b72249387c74db087b730c22e7368a7
Leela Lahari Vaka <leelalaharivaka@gmail.com>
2026-08-02 18:22:17 +0000

    Initial commit

---
+++ src/test/java/com/MonitorX/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MonitorX.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
