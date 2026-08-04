4495007619bbd5400507ebc150a6194066412f50
Rohil-Singhal-09 <rohil12345singhal@gmail.com>
2026-08-04 06:29:22 +0000

    Updated the Application file and added the Swagger UI to pom.xml for API documentation.

---
+++ src/main/java/com/MonitorX/MonitorXApplication.java
@@ -0,0 +1,11 @@
+package com.MonitorX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorXApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorXApplication.class, args);
+    }
+}
